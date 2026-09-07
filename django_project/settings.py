4ef5cc41f7e51b9fcd98a1c7de96aad6df5f93b5
Utsab <utsab2karki@gmail.com>
2026-09-07 22:18:03 +0545

    added articles

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     "accounts",
     "pages",
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
